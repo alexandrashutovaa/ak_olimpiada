8294f5edb984198eab08f44cbc8ea354bf7216ef
Your Name <you@example.com>
2025-10-03 21:32:40 +0300

    круговая орбита

--- kzadache.py
+++ kzadache.py
@@ -5,6 +5,7 @@ import math
 
 G = 0
 M = 0
+mu = 3.986e14
 def zadacha(x0, y0, z0, vx, vy, vz, dt, t0):
     t = []
     x = []
@@ -15,9 +16,9 @@ def zadacha(x0, y0, z0, vx, vy, vz, dt, t0):
         x_p = vx
         y_p = vy
         z_p = vz
-        vx_p = (G * M)/r**3 * x0
-        vy_p = (G * M)/r**3 * y0
-        vz_p = (G * M)/r**3 * z0
+        vx_p = -mu/r**3 * x0
+        vy_p = -mu/r**3 * y0
+        vz_p = -mu/r**3 * z0
         x_now = x0 + x_p * dt + (vx_p * dt**2)/2
         y_now = y0 + y_p * dt + (vy_p * dt**2)/2
         z_now = z0 + z_p * dt + (vz_p * dt**2)/2
@@ -33,14 +34,13 @@ def zadacha(x0, y0, z0, vx, vy, vz, dt, t0):
         vz += vz_p * dt
     return [[t], [x], [y], [z]]
 
-result = zadacha(1, 1, 1, 2, 3, 4, 1, 20)
+result = zadacha(6400e3, 0, 0, 0, 7700, 0, 1, 10000)
 
-fig = plt.figure(figsize=(10, 8))
-ax = fig.add_subplot(111, projection='3d')
+print(result[1])
+fig, ax = plt.subplots()
 
 # Поверхность
-ax.plot(result[1], result[2], result[3], 'b-', linewidth=2)
+ax.plot(result[1][0], result[2][0])
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
-ax.set_zlabel('Z')
 plt.show()
