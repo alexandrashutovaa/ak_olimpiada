5539cbe4ff0eebf42e055fbaa2bc1039c8b8ae60
Your Name <you@example.com>
2025-10-11 11:15:53 +0300

    проверка по времени и векторы

--- kzadache.py
+++ kzadache.py
@@ -6,41 +6,44 @@ import math
 G = 0
 M = 0
 mu = 3.986e14
-def zadacha(x0, y0, z0, vx, vy, vz, dt, t0):
+x0, y0, z0 = 6400e3, 0, 0,
+vector = np.array([x0, y0, z0])
+vx, vy, vz = 0, 7700, 0
+velocity = np.array([vx, vy, vz])
+
+def check_time(dt, t0):
+    if (t0 / dt) != (t0 // dt):
+        return t0 - (t0 // dt)*dt
+    else:
+        return 0
+    
+def zadacha(vector, velocity, dt, t0):
     t = []
-    x = []
-    y = []
-    z = []
-    for n in range (t0 // dt):
-        r = (x0**2 + y0**2 + z0**2)**0.5
-        x_p = vx
-        y_p = vy
-        z_p = vz
-        vx_p = -mu/r**3 * x0
-        vy_p = -mu/r**3 * y0
-        vz_p = -mu/r**3 * z0
-        x_now = x0 + x_p * dt + (vx_p * dt**2)/2
-        y_now = y0 + y_p * dt + (vy_p * dt**2)/2
-        z_now = z0 + z_p * dt + (vz_p * dt**2)/2
-        t += [dt * n]
-        x += [x_now]
-        y += [y_now]
-        z += [z_now]
-        x0 = x_now
-        y0 = y_now
-        z0 = z_now
-        vx += vx_p * dt
-        vy += vy_p * dt
-        vz += vz_p * dt
-    return [[t], [x], [y], [z]]
+    vectors = []
+    vector = vector.astype(np.float64)
+    velocity = velocity.astype(np.float64)
+    for n in range (int(t0 // dt)):
+        x, y, z = vector
+        r = (x**2 + y**2 + z**2)**0.5
+        vector_add = np.array([velocity[0]*dt, velocity[1]*dt, velocity[2]*dt])
+        aceleration = np.array([-mu/r**3 * vector[0], -mu/r**3 * vector[1], -mu/r**3 * vector[2]])
+        vector += vector_add
+        t.append(dt * n)
+        vectors.append(vector.copy())
+        velocity += aceleration*dt
 
-result = zadacha(6400e3, 0, 0, 0, 7700, 0, 1, 10000)
+    if check_time(dt, t0) != 0:
+        vectors.append(vector + np.array([vx*check_time(dt, t0), vy*check_time(dt, t0), vy*check_time(dt, t0)]))
+        t.append(t0 - check_time(dt, t0))
+    return t, vectors
 
-print(result[1])
+res_time, res_vector = zadacha(vector, velocity, 3, 10000)
+arr_x = [vec[0] for vec in res_vector]
+arr_y = [vec[1] for vec in res_vector]
 fig, ax = plt.subplots()
 
 # Поверхность
-ax.plot(result[1][0], result[2][0])
+ax.plot(arr_x, arr_y)
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 plt.show()
