a3a8ca42e0a542d01548f6f1b21bbd156bb3d5ea
Your Name <you@example.com>
2025-10-17 20:58:28 +0300

    ошибки исправлены, J2 нормальный

--- GRACEFO.py
+++ GRACEFO.py
@@ -9,15 +9,12 @@ def parse_gracefo_data(file_content):
     state_vectors = []
     
     for line in lines:
-        # Ищем строки с данными (начинаются с цифр)
         if line.strip() and line[0].isdigit():
             parts = line.split()
             
-            # Время (первая колонка)
             time = float(parts[0])
             times.append(time)
             
-            # Координаты (колонки 4-6) и скорости (колонки 10-12)
             xpos, ypos, zpos = float(parts[3]), float(parts[4]), float(parts[5])
             xvel, yvel, zvel = float(parts[9]), float(parts[10]), float(parts[11])
             
@@ -26,8 +23,8 @@ def parse_gracefo_data(file_content):
     return np.array(times), np.array(state_vectors)
 
 def calculate_kepler_elements(r, v, mu = 398600.4418):
-    r = np.array(r)  # Вектор положения [X, Y, Z]
-    v = np.array(v)  # Вектор скорости [VX, VY, VZ]
+    r = np.array(r) 
+    v = np.array(v)  
 
     L = np.cross(r, v)
     Lx, Ly, Lz = L
@@ -38,19 +35,15 @@ def calculate_kepler_elements(r, v, mu = 398600.4418):
     cosL = Lz/np.linalg.norm(L)
     sinL = ((Lx**2 + Ly**2)**0.5)/np.linalg.norm(L)
 
-    i = np.arctan2(sinL, cosL)  # i = atan2(sinL, cosL)
+    i = np.arctan2(sinL, cosL) 
     Omega = np.arctan2(Ny, Nx)
 
     mod_r = np.linalg.norm(r)
     mod_v = np.linalg.norm(v)
-    a = 1 / (2/mod_r - mod_v**2/mu)
     
-    e_vec = np.cross(v, L)/mu - r/np.linalg.norm(r)
-    e = np.linalg.norm(e_vec)
+    a = (mu * mod_r)/(2 * mu - mod_v**2 * mod_r)  
 
-    return Omega, i, a, e
-
-# ШАГ 3: ОСНОВНАЯ ПРОГРАММА
+    return Omega, i, a
 
 with open('gracefo.txt', 'r') as f:
     file_content = f.read()
@@ -61,21 +54,21 @@ times, state_vectors = parse_gracefo_data(file_content)
 Omega_res = []
 i_res = []
 a_res=[]
-e_res=[]
-mu=398600.4418
-Re = 6378.1
+e = 0
+mu=398600.4418e9
+Re = 6378.1e3
     
 for g in range(len(state_vectors)):
     r = state_vectors[g][:3]  
     v = state_vectors[g][3:]  
-    Omega, i, a, e = calculate_kepler_elements(r, v, mu)
+    Omega, i, a = calculate_kepler_elements(r, v, mu)
     Omega_res += [Omega]
     i_res += [i]
     a_res += [a]
-    e_res += [e]
 a = np.mean(a_res)
 i = np.mean(i_res)
-e = np.mean(e_res)
+
+
 
 fig, ax = plt.subplots()
     
@@ -85,7 +78,9 @@ ax.set_ylabel('Долгота восходящего узла')
 ax.grid(True, alpha=0.3)
 k, b = np.polyfit(times, Omega_res, 1)
 ax.plot(times, k*times + b, 'r-', linewidth=2, label=f'МНК: y={a:.4f}x+{b:.4f}')
-print(k)
+print("k = ", k)
+print("a = ", a)
+print("i = ", i)
 plt.show()
-j2 = (2 * k * a**2 * (1 - e**2)**2)/(-3 * (mu/a**3)**0.5 * Re**2)
-print(j2)
+j2 = (2 * k * a**2 * (1 - e**2)**2)/(-3 * (mu/a**3)**0.5 * Re**2 * math.cos(i))*10**3
+print("J2 = ", j2, " * 10^(-3)")
